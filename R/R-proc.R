5b4ef614a1ef3af33f0dbc589eb96b27c0d2c6cd
ElK1000o <102204400+ElK1000o@users.noreply.github.com>
2022-06-15 22:25:24 -0400

    ahjsgd

--- R/R-proc.R
+++ R/R-proc.R
@@ -35,12 +35,12 @@ data_proc = data%>%
                                TRUE~NA_character_),
          conf_dummy = case_when(confianza_6_j >=1 & confianza_6_j<=2 ~"Confianza",
                                 confianza_6_j >=3 & confianza_6_j<=4 ~"Desconfianza"),
-         edad = case_when(edad<=29 ~"Jovenes",
+         edad_tr = case_when(edad<=29 ~"Jovenes",
                           edad>=30 & edad<=59 ~"Adultos/as",
                           edad>=60 ~"Adulto/a Mayor",
                           TRUE~NA_character_),
          sexo = case_when(sexo==1 ~"Hombre", sexo==2 ~"Mujer"))%>%
-  select(sexo, edad, iden_pol, confianza, conf_dummy)
+  select(sexo, edad, edad_tr, iden_pol, confianza, conf_dummy)
 
 data_proc$iden_pol = factor(data_proc$iden_pol, levels = c(
   "Izquierda", "Centro Izquierda", "Centro", "Centro Derecha", "Derecha"))
@@ -54,10 +54,22 @@ frq(data_proc$edad)
 frq(data_proc$confianza)
 frq(data_proc$iden_pol)
 
+#MTC edad
+
+mean(data_proc$edad, na.rm = T)
+median(data_proc$edad)
+mlv(data_proc$edad, method = "mfv", na.rm = T)
+
+#DE edad
+sd(data_proc$edad, na.rm = T)
+
 #Graficos ----------------------------------------------------------------------
 
 plot_grpfrq(data_proc$iden_pol, data_proc$confianza,
             title = "Distribucion de niveles de Confianza según Identificacion Politica")
+
+plot_frq(data_proc$confianza,
+         title = "Distribucion Niveles de Confianza")
 
 plot_xtab(data_proc$confianza, data_proc$iden_pol, 
           title = "Grado de Confianza de cada Identificacion Politica")
@@ -68,7 +80,7 @@ plot_xtab(data_proc$conf_dummy, data_proc$iden_pol,
 plot_xtab(data_proc$sexo, data_proc$confianza,
             title = "Grado de Confianza según sexo")
 
-plot_xtab(data_proc$edad, data_proc$confianza,
+plot_xtab(data_proc$edad_tr, data_proc$confianza,
           title = "Grado de Confianza según Edad")
 
 #Guardar datos procesados ------------------------------------------------------
